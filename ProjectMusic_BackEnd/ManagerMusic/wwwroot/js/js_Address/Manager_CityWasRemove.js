e2df042c09d9001e94c8c7efd6e4fbe6a569ffba
Tuong.td <tuong.td@vn.vinx.asia>
2023-05-15 17:11:17 +0700

    update view get all city delete

--- ProjectMusic_BackEnd/ManagerMusic/wwwroot/js/js_Address/Manager_CityWasRemove.js
+++ ProjectMusic_BackEnd/ManagerMusic/wwwroot/js/js_Address/Manager_CityWasRemove.js
@@ -6,5 +6,21 @@ var PageIndex = 0;
 var PageSize = 20;
 //Load Citys delete
 function LoadCitysRemove() {
-
+    var count = 0;
+    $("#body_Citys").empty();
+    $("#TotalCitys").empty();
+    $("#TotalItem").empty();
+    $("#ItemInPage").empty();
+    $.ajax({
+        url: "/ManagerAddress/JsonGetAllCitysRemove",
+        type: "get",
+        data: {
+            pageIndex: PageIndex,
+            pageSize: PageSize,
+            seach: $("#TxtSeachValue").val()
+        },
+        success: function (result) {
+            console.log(result);
+        }
+    })
 }
